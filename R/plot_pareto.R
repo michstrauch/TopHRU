38845f7734147471db31fc905c61b30bea3fbe26
Christoph Schürz <christoph.schuerz@boku.ac.at>
2016-11-15 18:35:25 +0100

    Enhanced functionality with interactive visualization
    Included thresholds for aREA and n_HRU for better visualization of threshold combinations of interrest

--- R/plot_pareto.R
+++ R/plot_pareto.R
@@ -6,6 +6,7 @@
 #'   interactive plotly object helpful in selecting adequate threshold
 #'   combinations, or \code{FALSE} for a static ggplot object useful for
 #'   publication reasons.
+#' @import dplyr
 #' @import ggplot2
 #' @importFrom plotly plotly_build
 #'
@@ -16,45 +17,80 @@
 #' @export
 #'
 #' @examples
-plot_pareto <- function(hru_analysis, interactive = TRUE){
-  if(interactive){
-    pareto_ggplot <- ggplot() +
-      geom_line(data = hru_analysis$result_nondominated,
-                aes(x = n_HRU, y = aREA), col = "tomato3",
-                lwd = 0.3, alpha = 0.5) +
-      geom_point(data = hru_analysis$result_all,
-                 aes(x = n_HRU, y = aREA, col = Pareto_front),
-                 size = 0.7) +
-      theme_bw() +
-      scale_color_manual(values = c("grey70", "tomato3"))
-    pareto_plotly <- plotly_build(pareto_ggplot)
-    label_dom <- pareto_plotly$x$data[[2]]$text
-    label_dom <- sub("<br>Pareto_front: dominated","",label_dom)
-    label_dom <- paste0("thrs_comb: ",
-                        hru_analysis$result_all$thrs_comb[
-                          hru_analysis$result_all$Pareto_front == "dominated"],
-                        "<br>",
-                        label_dom)
-    label_nondom <- pareto_plotly$x$data[[3]]$text
-    label_nondom <- sub("<br>Pareto_front: non dominated","",label_nondom)
-    label_nondom <- paste0("thrs_comb: ",
-                           hru_analysis$result_all$thrs_comb[
-                             hru_analysis$result_all$Pareto_front == "non dominated"],
-                           "<br>",
-                           label_nondom)
-    pareto_plotly$x$data[[1]]$text <- ""
-    pareto_plotly$x$data[[2]]$text <- label_dom
-    pareto_plotly$x$data[[3]]$text <- label_nondom
-    pareto_plotly
-  } else {
-    pareto_ggplot <- ggplot() +
-      geom_line(data = hru_analysis$result_nondominated,
-                aes(x = n_HRU, y = aREA), col = "tomato3",
-                alpha = 0.5) +
-      geom_point(data = hru_analysis$result_all,
-                 aes(x = n_HRU, y = aREA, col = Pareto_front)) +
-      theme_bw() +
-      scale_color_manual(values = c("grey70", "tomato3"))
-    pareto_ggplot
+plot_pareto <- function(hru_analysis, aREA_thrs = NULL, HRU_thrs = NULL){
+  hru_analysis$result_all$Threshold <- factor(rep(2, nrow(hru_analysis$result_all)),
+                                              levels = 1:2, labels = c("above", "below"))
+  point_aes <- aes_string(x = "n_HRU", y = "aREA", col = "Pareto_front")
+  if(!is.null(aREA_thrs)){
+    hru_analysis$result_all$Threshold[hru_analysis$result_all$aREA
+                                      > aREA_thrs] <- "above"
+    point_aes <- aes_string(x = "n_HRU", y = "aREA",
+                            col = "Pareto_front",
+                            alpha = "Threshold")
   }
+  if(!is.null(HRU_thrs)){
+    hru_analysis$result_all$Threshold[hru_analysis$result_all$n_HRU
+                                      > HRU_thrs] <- "above"
+    point_aes <- aes_string(x = "n_HRU", y = "aREA",
+                            col = "Pareto_front",
+                            alpha = "Threshold")
+  }
+
+  pareto_ggplot <- ggplot() +
+    geom_line(data = hru_analysis$result_nondominated,
+              aes(x = n_HRU, y = aREA), col = "tomato3",
+              lwd = 0.3, alpha = 0.5) +
+    geom_point(data = hru_analysis$result_all, point_aes,
+               size = 0.7) +
+    theme_bw() +
+    scale_color_manual(values = c("grey70", "tomato3")) +
+    scale_alpha_discrete(drop = FALSE)
+
+  if(!is.null(HRU_thrs)){
+    pareto_ggplot <- pareto_ggplot +
+      geom_vline(aes(xintercept = HRU_thrs), linetype ="dashed", size = 0.3)
+  }
+  if(!is.null(aREA_thrs)){
+    pareto_ggplot <- pareto_ggplot +
+      geom_hline(aes(yintercept = aREA_thrs), linetype ="dashed", size = 0.3)
+  }
+
+
+  pareto_plotly <- plotly_build(pareto_ggplot)
+
+  pareto_lbl <- c("dominated", "non dominated")
+
+  for (i in 1:length(pareto_plotly$x$data)){
+    plotly_lbl <- pareto_plotly$x$data[[i]]$text[1] %>%
+      strsplit("<br>|:") %>%
+      unlist %>%
+      gsub("^\\s+|\\s+$", "", .)
+
+    pareto_filt <- pareto_lbl[pareto_lbl %in% plotly_lbl]
+    if("above" %in% plotly_lbl){
+      thrs_filt <- "above"
+    } else{
+      thrs_filt   <- "below"
+    }
+
+    if(length(pareto_filt) > 0){
+      thrs_comb_txt <- hru_analysis$result_all %>%
+        filter(Threshold == thrs_filt,
+               Pareto_front == pareto_filt) %>%
+        .$thrs_comb
+
+      label_txt <- pareto_plotly$x$data[[i]]$text
+      label_txt <- sub("Pareto_front: dominated","",label_txt)
+      label_txt <- sub("Pareto_front: non dominated","",label_txt)
+      label_txt <- sub("Threshold: below","",label_txt)
+      label_txt <- sub("Threshold: above","",label_txt)
+      label_txt <- sub("<br><br>","<br>",label_txt)
+      label_txt <- sub("<br><br>","<br>",label_txt)
+      label_txt <- paste0("thrs_comb: ",thrs_comb_txt, label_txt)
+      pareto_plotly$x$data[[i]]$text <- label_txt
+    }
+
+    rm(pareto_filt)
+  }
+  pareto_plotly
 }
