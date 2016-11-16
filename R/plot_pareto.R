0ab7b79f2481ab5a7b46f91585d01d0b8992ea06
Christoph Schürz <christoph.schuerz@boku.ac.at>
2016-11-16 15:23:22 +0100

    modified visualization, overcome problems with overlain points

--- R/plot_pareto.R
+++ R/plot_pareto.R
@@ -18,34 +18,48 @@
 #'
 #' @examples
 plot_pareto <- function(hru_analysis, aREA_thrs = NULL, HRU_thrs = NULL){
-  hru_analysis$result_all$Threshold <- factor(rep(2, nrow(hru_analysis$result_all)),
-                                              levels = 1:2, labels = c("above", "below"))
-  point_aes <- aes_string(x = "n_HRU", y = "aREA", col = "Pareto_front")
+  hru_analysis$result_all$Threshold <-
+    factor(rep(2, nrow(hru_analysis$result_all)),
+           levels = 1:2, labels = c("above", "below"))
+
   if(!is.null(aREA_thrs)){
-    hru_analysis$result_all$Threshold[hru_analysis$result_all$aREA
-                                      > aREA_thrs] <- "above"
-    point_aes <- aes_string(x = "n_HRU", y = "aREA",
-                            col = "Pareto_front",
-                            alpha = "Threshold")
+    hru_analysis$result_all$Threshold[
+      hru_analysis$result_all$aREA > aREA_thrs] <- "above"
   }
   if(!is.null(HRU_thrs)){
-    hru_analysis$result_all$Threshold[hru_analysis$result_all$n_HRU
-                                      > HRU_thrs] <- "above"
-    point_aes <- aes_string(x = "n_HRU", y = "aREA",
-                            col = "Pareto_front",
-                            alpha = "Threshold")
+    hru_analysis$result_all$Threshold[
+      hru_analysis$result_all$n_HRU > HRU_thrs] <- "above"
+  }
+  if(any(hru_analysis$result_all$Threshold == "above")){
+    hru_analysis$result_all$Legend <- paste(
+      hru_analysis$result_all$Pareto_front,
+      hru_analysis$result_all$Threshold, sep = ", ") %>%
+      ordered(., levels = c("non dominated, below",
+                            "dominated, below",
+                            "non dominated, above",
+                            "dominated, above"))
+  } else {
+    hru_analysis$result_all$Legend <-
+      hru_analysis$result_all$Pareto_front %>%
+      ordered(., levels = c("non dominated", "dominated"))
   }
 
   pareto_ggplot <- ggplot() +
     geom_line(data = hru_analysis$result_nondominated,
               aes(x = n_HRU, y = aREA), col = "tomato3",
               lwd = 0.3, alpha = 0.5) +
-    geom_point(data = hru_analysis$result_all, point_aes,
+    geom_point(data = hru_analysis$result_all,
+               aes(x = n_HRU, y = aREA, col = Legend),
                size = 0.7) +
-    theme_bw() +
-    scale_color_manual(values = c("grey70", "tomato3")) +
-    scale_alpha_discrete(drop = FALSE)
+    theme_bw()
 
+  if(any(hru_analysis$result_all$Threshold == "above")){
+    pareto_ggplot <- pareto_ggplot +
+    scale_color_manual(values = c("tomato3", "grey65", "tomato1", "grey90"), drop = FALSE)
+  } else {
+    pareto_ggplot <- pareto_ggplot +
+      scale_color_manual(values = c("tomato3", "grey65"), drop = FALSE)
+  }
   if(!is.null(HRU_thrs)){
     pareto_ggplot <- pareto_ggplot +
       geom_vline(aes(xintercept = HRU_thrs), linetype ="dashed", size = 0.3)
@@ -58,7 +72,9 @@ plot_pareto <- function(hru_analysis, aREA_thrs = NULL, HRU_thrs = NULL){
 
   pareto_plotly <- plotly_build(pareto_ggplot)
 
-  pareto_lbl <- c("dominated", "non dominated")
+  legend_lbl <- c("dominated", "non dominated",
+                  "dominated, above", "non dominated, above",
+                  "dominated, below", "non dominated, below")
 
   for (i in 1:length(pareto_plotly$x$data)){
     plotly_lbl <- pareto_plotly$x$data[[i]]$text[1] %>%
@@ -66,31 +82,24 @@ plot_pareto <- function(hru_analysis, aREA_thrs = NULL, HRU_thrs = NULL){
       unlist %>%
       gsub("^\\s+|\\s+$", "", .)
 
-    pareto_filt <- pareto_lbl[pareto_lbl %in% plotly_lbl]
-    if("above" %in% plotly_lbl){
-      thrs_filt <- "above"
-    } else{
-      thrs_filt   <- "below"
-    }
+    legend_filt <- legend_lbl[legend_lbl %in% plotly_lbl]
 
-    if(length(pareto_filt) > 0){
+
+    if(length(legend_filt) > 0){
       thrs_comb_txt <- hru_analysis$result_all %>%
-        filter(Threshold == thrs_filt,
-               Pareto_front == pareto_filt) %>%
+        filter(Legend == legend_filt) %>%
         .$thrs_comb
 
       label_txt <- pareto_plotly$x$data[[i]]$text
-      label_txt <- sub("Pareto_front: dominated","",label_txt)
-      label_txt <- sub("Pareto_front: non dominated","",label_txt)
-      label_txt <- sub("Threshold: below","",label_txt)
-      label_txt <- sub("Threshold: above","",label_txt)
-      label_txt <- sub("<br><br>","<br>",label_txt)
-      label_txt <- sub("<br><br>","<br>",label_txt)
-      label_txt <- paste0("thrs_comb: ",thrs_comb_txt, label_txt)
+      for (rmv in legend_lbl){
+        label_txt <- sub(paste0("<br>Legend: ", rmv),"",label_txt)
+      }
+
+      label_txt <- paste0("thrs_comb: ",thrs_comb_txt, "<br>", label_txt)
       pareto_plotly$x$data[[i]]$text <- label_txt
     }
 
-    rm(pareto_filt)
+    rm(legend_filt)
   }
   pareto_plotly
 }
