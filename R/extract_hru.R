ea46f7c43464108b493e0e41186bfbc07371f2b7
Christoph Schuerz <christoph.schuerz@boku.ac.at>
2017-01-07 21:26:36 +0100

    package updated. RODBC now only suggested

--- R/extract_hru.R
+++ R/extract_hru.R
@@ -4,15 +4,18 @@
 #'   file to read must be either the project.mdb data base file or a .csv
 #'   file to where the hrus table from the project.mdb data base was saved.
 #'
-#' @importFrom RODBC odbcConnectAccess sqlQuery
-#'
 #' @return Returns the hru_table as a data.frame
 #' @export
 
 extract_HRU <- function(file_path) {
   if(grepl(".mdb$",file_path)){
-    mdb_con <- odbcConnectAccess(file_path)
-    hru_table <- sqlQuery( mdb_con , paste("select * from hrus"))
+    if (requireNamespace("RODBC", quietly = TRUE)) {
+      mdb_con <- RODBC::odbcConnectAccess(file_path)
+      hru_table <- RODBC::sqlQuery( mdb_con , paste("select * from hrus"))
+    } else {
+      stop("RODBC required if hrus table is loaded from 'project.mdb'",
+           call. = FALSE)
+      }
   } else if(grepl(".csv$",file_path)){
     hru_table <- read.csv(file = file_path)
   } else{
